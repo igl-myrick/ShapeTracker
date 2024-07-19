e3fc1afe06ada29d0cd4cfa7d2426b8f44662651
igl-myrick <igl.myrick@gmail.com>
2024-07-19 15:06:22 -0700

    add constructor values

--- ShapeTracker/Models/Triangle.cs
+++ ShapeTracker/Models/Triangle.cs
@@ -2,6 +2,15 @@ namespace ShapeTracker.Models
 {
   public class Triangle
   {
+    public int Side1;
+    public int Side2;
+    public int Side3;
 
+    public Triangle(int length1, int length2, int length3)
+    {
+      Side1 = length1;
+      Side2 = length2;
+      Side3 = length3;
+    }
   }
 }
