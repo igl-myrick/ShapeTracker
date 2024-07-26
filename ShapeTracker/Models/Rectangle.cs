712662ad8968508474791e9b980347dbedd174b0
igl-myrick <igl.myrick@gmail.com>
2024-07-26 15:42:40 -0700

    add side2 field

--- ShapeTracker/Models/Rectangle.cs
+++ ShapeTracker/Models/Rectangle.cs
@@ -3,10 +3,12 @@ namespace ShapeTracker.Models
   public class Rectangle
   {
     public int Side1 { get; }
+    public int Side2 { get; }
 
-    public Rectangle(int length1)
+    public Rectangle(int length1, int length2)
     {
       Side1 = length1;
+      Side2 = length2;
     }
   }
 }
